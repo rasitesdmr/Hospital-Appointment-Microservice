c42b9a43f05bc4e039460146d39cc81432d4d5af
rasitesdmr <mrasitesdemir7@gmail.com>
2023-05-13 14:17:30 +0300

    excelservice hata düzeltme

--- hospital-service/src/main/java/com/rasitesdmr/hospitalservice/consumer/KafkaConsumer.java
+++ hospital-service/src/main/java/com/rasitesdmr/hospitalservice/consumer/KafkaConsumer.java
@@ -2,7 +2,6 @@ package com.rasitesdmr.hospitalservice.consumer;
 
 import com.rasitesdmr.hospitalservice.service.*;
 import kafka.model.dto.response.*;
-import kafka.model.response.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.PartitionOffset;
